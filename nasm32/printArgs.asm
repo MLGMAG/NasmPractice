dcf7457a4251c05cedf410841055b85062b7a7f1
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2021-03-22 13:57:01 +0200

    [Args] Print args, that were passed

---
+++ nasm32/printArgs.asm
@@ -0,0 +1,20 @@
+%include	'functions.asm'
+
+section .text
+global _start
+
+_start:
+	
+	pop	ecx
+	
+getArg:
+	cmp	ecx, 0h
+	jz	noMoreArgs
+	pop	eax
+	call	sprintLF
+	dec	ecx
+	jmp	getArg
+
+noMoreArgs:
+	call	quit	
+
